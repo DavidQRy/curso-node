e2ce6fefba2e771cdd8c0f0681bec82070f93cea
DavidQRy <davidgui393@gmail.com>
2025-06-25 21:54:53 -0500

    Finalizacion clase 7

--- clase-7/config.js
+++ clase-7/config.js
@@ -1,4 +1,5 @@
 export const {
     PORT = 3000,
-    SALT_ROUNDS = 8
+    SALT_ROUNDS = 8,
+    SECRET_JWT_KEY = '94bbd486-128f-4b91-a862-e81116a3b088'
 } = process.env
--- clase-7/index.js
+++ clase-7/index.js
@@ -1,22 +1,51 @@
 import express, {json} from 'express'
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
 
-import { PORT } from './config.js'
+import { PORT, SECRET_JWT_KEY } from './config.js'
 import { userRepository } from './user-repository.js'
 
 const app = express()
 
 app.set('view engine', 'ejs')
+
+app.use(cookieParser())
 app.use(json())
 
+app.use( (req, res, next) => {
+    const token =req.cookies.access_token
+    req.session = {user: null}
+
+    try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    req.session.user = data
+    } catch (error) {
+        req.session.user = null
+    }
+
+    next()
+})
+
 app.get('/', (req, res) => {
-    res.render('examples', {username: 'David'})
+    const {user} = req.session
+    res.render('index', {user})
 })
 
 app.post('/login', async (req, res) =>{
     const {username, password} = req.body
     try {
         const user = await userRepository.login({username, password})
-        res.render('examples', {username: 'David'})
+        const token = jwt.sign({id: user._id, username: user.user},
+            SECRET_JWT_KEY,
+            {
+                expiresIn: '1h'
+            })
+        res.cookie('access_token', token, {
+            httpOnly: true, // la cookie solo se puede acceder en el servidor
+            secure: process.env.NODE_ENV === 'production', // la cookie solo se puede acceder en el servidor
+            sameSite: 'strict', // La cookie solo se puede acceder en el mismo dominio
+            maxAge: 1000 * 60 * 60 // La cookie tiene un tiempo de validez de 1 hora
+        }).send({username: user.username})
     } catch (error) {
         res.status(401).send(error.message)
     }
@@ -33,7 +62,16 @@ app.post('/register',async (req, res) => {
 })
 
 app.post('/logout', (req, res) => {
+    res.clearCookie('access_token')
+    .json({message: 'Logout succesful'})
+})
 
+app.get('/protected', (req, res) => {
+    const {user} = req.session
+    if (!user) {
+        return res.status(403).send('Access not authorized')
+    }
+    res.render('protected', {user})
 })
 
 app.listen(PORT, () => {
